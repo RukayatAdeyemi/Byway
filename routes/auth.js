af398c3f01c0a69b6464e9abb1a15b375998f51f
Rukayat Adeyemi Abiodun <rukayatadeyemiabiodun@gmail.com>
2025-11-06 13:28:27 -0800

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const {signup, login, getCurrentUser, updateProfile, updatePassword} = require("../controllers/authController");
+const {signup, login, getCurrentUser, updateProfile, updatePassword} = require("./../controllers/authController");
 
 // Declare the validation rules
 const {
